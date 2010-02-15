07869f3a0460d28860f0d6e0687782d0869e25e8
Ben Newman <newmanb@nulletch64.dreamhost.com>
2010-02-15 13:41:25 -0800

    aegis/lang/str.camelize

--- lang/str.js
+++ lang/str.js
@@ -1,8 +1,16 @@
 var excess_pattern = /^\s+|\s+$/g,
-    regexp_pattern = /([.*+?^=!:${}()|[\]\/\\])/g;
+    regexp_pattern = /([.*+?^=!:${}()|[\]\/\\])/g,
+    camel_pattern = /-(\w)/g;
+
 exports.escapeRegExp = function(str) {
     return (str+"").replace(regexp_pattern, "\\$1");
 };
+
 exports.strip = function(str) {
     return (str+"").replace(excess_pattern, "");
+};
+
+function hump(_, c) { return c.toUpperCase() }
+exports.camelize = function(str) {
+    return str.replace(camel_pattern, hump);
 };
